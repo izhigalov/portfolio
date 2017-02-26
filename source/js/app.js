af731f7411c20a8e0c9e245bda92bbdff10e2f06
izhigalov <rim10@inbox.ru>
2017-02-26 20:32:30 +0400

    25commit

--- source/js/app.js
+++ source/js/app.js
@@ -5,3 +5,13 @@
     document.querySelector('.greating_picture').classList.add('m--show');
   }, 1000);
 })();
+
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 56.862230, lng: 53.217372},
+        zoom: 12
+           //styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}]
+            });
+}
